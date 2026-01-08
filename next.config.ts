5e452d35036dc3a7d396b76c4259884504d340b0
Nim-os <10841304+Nim-os@users.noreply.github.com>
2026-01-08 12:27:57 -0500

    Fix social tracking redirects

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,31 @@ const { withPlausibleProxy } = require('next-plausible');
 
 const nextConfig: NextConfig = withPlausibleProxy()({
   /* config options here */
+
+  async redirects() {
+    return [
+      {
+        source: '/ig',
+        destination: '/?utm_source=instagram&utm_medium=social&utm_campaign=beta',
+        permanent: false,
+      },
+      {
+        source: '/tt',
+        destination: '/?utm_source=tiktok&utm_medium=social&utm_campaign=beta',
+        permanent: false,
+      },
+      {
+        source: '/fb',
+        destination: '/?utm_source=facebook&utm_medium=social&utm_campaign=beta',
+        permanent: false,
+      },
+      {
+        source: '/yt',
+        destination: '/?utm_source=youtube&utm_medium=social&utm_campaign=beta',
+        permanent: false,
+      }
+    ]
+  },
 });
 
 export default nextConfig;
