ea941bdea308a36e0a518cd4cb674b0b011818e0
Nim-os <10841304+Nim-os@users.noreply.github.com>
2026-01-24 18:08:37 -0500

    Add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,13 +30,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const FB_PIXEL_ID = process.env.FACEBOOK_PIXEL_ID;
+  const GTM_ID = process.env.GTM_ID;
 
   const plausible = usePlausible();
 
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <Script id='fb-pixel' strategy='afterInteractive'>
+        {FB_PIXEL_ID &&
+          <Script id='fb-pixel' strategy='afterInteractive'>
             {`
               !function(f,b,e,v,n,t,s)
               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -50,17 +52,39 @@ export default function RootLayout({
               fbq('track', 'PageView');
             `}
           </Script>
+        }
+        {GTM_ID &&
+          <Script
+            id="gtm-head-script"
+            strategy="beforeInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`,
+            }}
+          />
+        }
 
         <PlausibleProvider domain="thecherami.com">
           <Header />
 
           <main className="pt-16 bg-[#FCFBF8]">
-            <noscript>
-              <img
-                height="1" width="1" style={{ display: 'none' }}
-                src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
-              />
-            </noscript>
+            {FB_PIXEL_ID &&
+              <noscript>
+                <img
+                  height="1" width="1" style={{ display: 'none' }}
+                  src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+                />
+              </noscript>
+            }
+            {GTM_ID &&
+              <noscript>
+                <iframe
+                  src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                  height="0"
+                  width="0"
+                  style={{ display: 'none', visibility: 'hidden' }}
+                />
+              </noscript>
+            }
             {children}
           </main>
 
