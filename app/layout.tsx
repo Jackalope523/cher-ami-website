2db60703b72bf745d5ab516f7d5c411ea49c2784
Nim-os <10841304+Nim-os@users.noreply.github.com>
2026-01-24 18:28:18 -0500

    Remove tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ import PlausibleProvider, { usePlausible } from 'next-plausible';
 import './globals.css';
 import CTA from '@/components/CTA';
 import Header from '@/components/Header';
-import Script from 'next/script';
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700'],
@@ -29,62 +28,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const FB_PIXEL_ID = process.env.FACEBOOK_PIXEL_ID;
-  const GTM_ID = process.env.GTM_ID;
-
   const plausible = usePlausible();
 
   return (
     <html lang="en">
       <body className={poppins.className}>
-        {FB_PIXEL_ID &&
-          <Script id='fb-pixel' strategy='afterInteractive'>
-            {`
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${FB_PIXEL_ID}');
-              fbq('track', 'PageView');
-            `}
-          </Script>
-        }
-        {GTM_ID &&
-          <Script
-            id="gtm-head-script"
-            strategy="beforeInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`,
-            }}
-          />
-        }
 
         <PlausibleProvider domain="thecherami.com">
           <Header />
 
           <main className="pt-16 bg-[#FCFBF8]">
-            {FB_PIXEL_ID &&
-              <noscript>
-                <img
-                  height="1" width="1" style={{ display: 'none' }}
-                  src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
-                />
-              </noscript>
-            }
-            {GTM_ID &&
-              <noscript>
-                <iframe
-                  src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-                  height="0"
-                  width="0"
-                  style={{ display: 'none', visibility: 'hidden' }}
-                />
-              </noscript>
-            }
             {children}
           </main>
 
