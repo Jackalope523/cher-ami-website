51f758a338dd8d1ddebf0fd5e75df330c8548f92
Nim-os <10841304+Nim-os@users.noreply.github.com>
2026-01-24 18:28:26 -0500

    OCD

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-
         <PlausibleProvider domain="thecherami.com">
           <Header />
 
