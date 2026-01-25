83b95bcaaa7771eb74413ba858459831f81698ac
Nim-os <10841304+Nim-os@users.noreply.github.com>
2026-01-24 19:15:46 -0500

    Add more priorities

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,6 +8,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 1,
     },
+    
     {
       url: 'https://thecherami.com/help',
       lastModified: new Date(),
@@ -15,10 +16,60 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: 'https://thecherami.com/help/general',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://thecherami.com/help/account',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://thecherami.com/help/privacy',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://thecherami.com/help/billing',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+
+    {
       url: 'https://thecherami.com/contact',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.7,
+    },
+
+    {
+      url: 'https://thecherami.com/legal',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://thecherami.com/legal/terms',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://thecherami.com/legal/privacy',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://thecherami.com/legal/return',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
     },
   ]
 }
