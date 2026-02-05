aa7414345a2083a9d81be119f0edde51ad56d041
Nim-os <10841304+Nim-os@users.noreply.github.com>
2026-02-05 09:34:02 -0500

    Add back meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import PlausibleProvider, { usePlausible } from 'next-plausible';
 import './globals.css';
 import CTA from '@/components/CTA';
 import Header from '@/components/Header';
+import Script from 'next/script';
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700'],
@@ -28,15 +29,40 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const plausible = usePlausible();
+  const FB_PIXEL_ID = process.env.FACEBOOK_PIXEL_ID;
 
   return (
     <html lang="en">
       <body className={poppins.className}>
+        {FB_PIXEL_ID &&
+          <Script id='fb-pixel' strategy='afterInteractive'>
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${FB_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+        }
+
         <PlausibleProvider domain="thecherami.com">
           <Header />
 
           <main className="pt-16 bg-[#FCFBF8]">
+            {FB_PIXEL_ID &&
+              <noscript>
+                <img
+                  height="1" width="1" style={{ display: 'none' }}
+                  src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+                />
+              </noscript>
+            }
             {children}
           </main>
 
