6150ce6d2506e470961e702d4979d33c5a83d83b
Nim-os <10841304+Nim-os@users.noreply.github.com>
2026-03-02 13:47:36 +0100

    Change banner offer

--- components/Header.tsx
+++ components/Header.tsx
@@ -137,7 +137,7 @@ export default function Header() {
           onClick={() => plausible('Promo Banner Pressed')}
           className="flex flex-col gap-1 py-2 px-5 lg:px-13 xl:px-5">
           <p className="text-[.9rem] text-[#FCFBF8] text-center font-medium">
-            February Offer: Your first magazine free!{' '}
+            Early Bird Offer: Your first magazine free!{' '}
 
             <span className="text-[.7rem] text-[#FCFBF8] text-center font-normal">
               (max 1 per circle)
