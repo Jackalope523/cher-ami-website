9ae964544925f0b0611f209dc4e5e9ea524d43eb
EthanCote5233 <ECote523@Gmail.com>
2026-03-06 10:47:40 -0500

    Add prospective user creation to quiz route

--- app/api/quiz/route.ts
+++ app/api/quiz/route.ts
@@ -4,9 +4,20 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST(request: NextRequest) {
   const ONE_SIGNAL_API_KEY = process.env.ONE_SIGNAL_API_KEY;
   const ONE_SIGNAL_APP_ID = process.env.ONE_SIGNAL_APP_ID;
+  const CHER_AMI_API_URL = process.env.CHER_AMI_API_URL;
+  const CHER_AMI_API_KEY = process.env.CHER_AMI_API_KEY;
 
   const answers: QuizAnswers = await request.json();
   
+  await fetch(`${CHER_AMI_API_URL}/website/user`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `key ${CHER_AMI_API_KEY}`,
+    },
+    body: JSON.stringify({ email: answers.email }),
+  });
+
   const response = await fetch(`https://api.onesignal.com/apps/${ONE_SIGNAL_APP_ID}/users`, {
     method: 'POST',
     headers: {
