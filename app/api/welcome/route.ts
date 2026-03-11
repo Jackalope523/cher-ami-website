23d0323f27132d6cb69de295ee902fd75c2342c1
Nim-os <10841304+Nim-os@users.noreply.github.com>
2026-03-11 12:57:27 -0400

    Remove welcome

--- app/api/welcome/route.ts
+++ app/api/welcome/route.ts
@@ -17,38 +17,5 @@ export async function POST(request: NextRequest) {
       body: JSON.stringify({ email }),
   });
 
-  
-
-  const tags: any = {
-    'entry': 'web',
-  }
-
-  if (military) {
-    tags['military'] = '1';
-  }
-
-  const response = await fetch("https://api.onesignal.com/notifications?c=email", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `key ${ONE_SIGNAL_API_KEY}`,
-    },
-    body: JSON.stringify({
-      app_id: ONE_SIGNAL_APP_ID,
-      email_to: [email],
-      email_subject: 'Welcome!',
-      template_id: '21bd2cb8-022e-4151-be65-ea80356ae8e0' // welcome-v1
-    }),
-  });
-
-  const data = await response.json();
-
-  if (!response.ok) {
-    console.log("FAILURE!!!!");
-    console.log("HTTP status:", response.status);
-    console.log("OneSignal errors:", data.errors);
-    throw new Error(`Response status: ${response.status}`);
-  }
-
-  return NextResponse.json(data);
+  return NextResponse.json({});
 }
